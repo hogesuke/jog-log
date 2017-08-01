4cf49c1c6b8025ec1ac15884555c6d505ad9c7d0
hogesuke <miyado@gmail.com>
2017-08-01 14:02:43 +0900

    コンテンツを中央配置するように修正

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,12 @@ export default {
 
 <style>
 #app {
+  width: 800px;
+  margin: 0 auto;
+  color: #2c3e50;
+  text-align: center;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
